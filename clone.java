0a250e65d9da3ab4c90b6e883fc8318d6aaf0bf1
Pratush Singh <120071839+Pratyush8055@users.noreply.github.com>
2023-05-03 14:31:50 +0530

    Create clone.java

---
+++ clone.java
@@ -0,0 +1,33 @@
+package pratyush;
+class Test 
+{
+	int x,y;
+	
+}
+class Test2 implements Cloneable
+{
+	int a,b;
+	Test c= new Test();
+	public Object clone() throws CloneNotSupportedException
+	{
+		return super.clone();
+	}
+}
+public class Test3 {
+
+	public static void main(String[] args) throws CloneNotSupportedException
+	{
+		Test2 t =new Test2();// TODO Auto-generated method stub
+		t.a=10;
+		t.b=20;
+		t.c.x=30;
+		t.c.y=40;
+		Test2 t2=(Test2)t.clone();
+		t2.a=100;
+		t2.c.x=300;
+		t2.c.y=700;
+		System.out.println(t.a+" "+t.b+" "+t.c.x+" "+t.c.y);
+		System.out.println(t2.a+" "+t2.b+" "+t2.c.x+" "+t2.c.y);
+	}
+
+}
